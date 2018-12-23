3065f4e008203e27cd56afd764b3448b273ece07
bugaga1231 <45533221+bugaga1231@users.noreply.github.com>
2018-12-23 10:51:05 +0300

    Update lab05.asm

--- lab05.asm
+++ lab05.asm
@@ -18,6 +18,9 @@ DATA SEGMENT
 	min_price	dw	0
 	s1	db	'Vvedite kolichestvo knig$'
 	s2	db	'Vvedite title, second_name,price,year$'
+	
+	count_O	dw	0
+	two	dw	2
 DATA ENDS
 
 
@@ -39,43 +42,76 @@ Start proc
 	mov	cx,	n
 	mov	bp,	0
 	
-	enter_book:
-		mov	ah,	9h
-		lea	dx,	s2
-		int	21h
-		outch 20h
-		push	cx
-		mov	ah,	3fh
-		mov	cx,	20
-		mov	bx,	0
-		lea	dx,	book[bp].title_
-		int	21h
-		mov	ah,	3fh
-		mov	cx,	20
-		mov	bx,	0
-		lea	dx,	book[bp].second_name
-		int	21h
-		inint	book[bp].price
-		inint	book[bp].year
-		pop	cx
-		add	bp,	type	info
-	loop enter_book
+enter_book:
+	mov	ah,	9h
+	lea	dx,	s2
+	int	21h
+	outch 20h
+	push	cx
+	mov	ah,	3fh
+	mov	cx,	20
+	mov	bx,	0
+	lea	dx,	book[bp].title_
+	int	21h
+	mov	ah,	3fh
+	mov	cx,	20
+	mov	bx,	0
+	lea	dx,	book[bp].second_name
+	int	21h
+	inint	book[bp].price
+	inint	book[bp].year
+	pop	cx
+	add	bp,	type	info
+loop enter_book
 	
 	mov	cx,	n
 	mov	bp,	0
 	mov	ax,	book[bp].price
 	mov	min_price,	ax
-	findMin_price:
-		add	bp,	type	info
-		mov	ax,	book[bp].price
-		cmp	min_price,	ax
-		jl	L
-		mov	min_price,	ax
-	L:	loop	findMin_price
+findMin_price:
+	add	bp,	type	info
+	mov	ax,	book[bp].price
+	cmp	min_price,	ax
+	jl	L
+	mov	min_price,	ax
+L:	loop	findMin_price
 	
 	outint	min_price
+	mov	ax,	min_price
+	mul	two
+	mov	min_price,	ax
+	
+	mov	cx,	n
+	mov	bx,	0
+find_author:
+	push	cx
+	mov	count_O,	0
+	lea	di,	book[bx].title_
+	mov	cx,	20
+	mov	al,	'o'
+L3:	repne	scasb
+	je	L1
+	inc	count_O
+	jmp	L3
+L1:	cmp	count_O,	2
+	jg	L2
+	mov	ax,	book[bx].price
+	cmp	ax,	min_price
+	jle	L2
+	mov	ah,	9h
+	lea	dx,	book[bx].second_name
+	int	21h
+	outch	20h
+L2:	pop	cx	
+	add	bp,	type	info
+loop	find_author
 ;========== Заканчивайте писать Ваш код======================
 	pop	ax
 	pop	ds
 mov ah, 4ch
   int 21h
+
+start endp
+CODE ENDS
+
+END	Start
